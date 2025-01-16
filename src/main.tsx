eb6821fd45b52b6a574c19bf20fe3ee387d4ecbe
Kenny Morelo <kamt1128@gmail.com>
2025-01-15 23:41:29 -0500

    Fix structuredClone is not defined in application build

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,5 @@
+import 'core-js/actual/structured-clone';
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
